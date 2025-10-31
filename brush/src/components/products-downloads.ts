ee4f939ccd4ff4dd17539271d351ee9eefcc4db6
Hervé Guétin <herve.guetin@dnd.fr>
2025-10-31 21:53:29 +0100

    PDF download

--- brush/src/components/products-downloads.ts
+++ brush/src/components/products-downloads.ts
@@ -3,5 +3,11 @@ export default function () {
     login() {
       return Brush.Referrals.makeLoginRedirectUrl();
     },
+
+    downloadProductPdf(productId: number) {
+      const button = this.$el as HTMLButtonElement;
+      button.disabled = true;
+      window.location.href = `/apps/brush/product-download/${productId}`;
+    },
   }));
 }
